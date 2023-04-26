67a70fba2f7d75fd3b7d77847a457fef9b89689e
bdf007 <bdf007@gmail.com>
2023-04-26 12:17:08 +0200

    create a ball prefab and a gameController object

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject ballPrefab;
+
+    private Ball currentBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, transform);
+
+        currentBall = ballInstance.GetComponent<Ball>();
+        currentBall.transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
